c03c0ab844ae11cd2f08fb4522c80403550e21ea
leijian <leijian@bdqn.com>
2023-11-19 16:27:07 +0800

    招投标模块框架搭建

--- ruoyi-modules/hh-bidding/src/main/java/com/hh/pms/controller/BidGetTenderController.java
+++ ruoyi-modules/hh-bidding/src/main/java/com/hh/pms/controller/BidGetTenderController.java
@@ -29,7 +29,7 @@ import com.ruoyi.common.core.web.page.TableDataInfo;
  * @date 2023-11-19
  */
 @RestController
-@RequestMapping("/tender")
+@RequestMapping("/tenderFile")
 public class BidGetTenderController extends BaseController
 {
     @Autowired
