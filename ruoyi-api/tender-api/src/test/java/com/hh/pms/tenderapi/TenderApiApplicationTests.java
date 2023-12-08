0998d5f197fce4970f0724e340604799e586f259
leijian <leijian@bdqn.com>
2023-12-08 17:09:42 +0800

    招标---n+3

---
+++ ruoyi-api/tender-api/src/test/java/com/hh/pms/tenderapi/TenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.pms.tenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
