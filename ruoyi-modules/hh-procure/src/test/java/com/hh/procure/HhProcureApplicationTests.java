9c153803980fc52ea44e10dbdeed94e9eaf4ad79
Chen <3087401763@qq.com>
2023-12-18 10:08:24 +0800

    修改模块

---
+++ ruoyi-modules/hh-procure/src/test/java/com/hh/procure/HhProcureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.procure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhProcureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
